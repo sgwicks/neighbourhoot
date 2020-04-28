876978381f8a46f7782a5866d2fc7d027b154bff
sgwicks <samgwicks@gmail.com>
2020-04-28 15:54:54 +0100

    Add birdQuery template

---
+++ birdQuery.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'eu-west-2',
+  endpoint: 'https://dynamodb.eu-west-2.amazonaws.com'
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+  TableName: 'Birds',
+  KeyConditionExpression: '#Location = :Location AND #Bird_id = :Bird_id',
+
+  ExpressionAttributeNames: {
+    '#Location': 'Location',
+    '#Bird_id': 'Bird_id'
+  },
+  ExpressionAttributeValues: {
+    ':Location': 'North Leeds',
+    ':Bird_id': 1
+  }
+};
+
+docClient.query(params, (err, data) => {
+  if (err) {
+    console.error('error', JSON.stringify(err, null, 2));
+  } else {
+    console.log('heres ya queries', JSON.stringify(data, null, 2));
+  }
+});

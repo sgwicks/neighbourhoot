fb84b0d40a253f26247a839ed79618e1df9e5067
s-alex-smith <salexsmith91@gmail.com>
2020-04-29 11:09:47 +0100

    updated login screen to change state of email address and password, also made password secretive

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -5,35 +5,55 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  ScrollView,
+  Button,
 } from "react-native";
-
-import { onChange } from "react-native-reanimated";
 
 const LoginScreen = ({ navigation }) => {
   const { navigate } = navigation;
-  const [value, onChangeText] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const submit = () => {
+    const userLogin = {
+      email,
+      password,
+    };
+    setSubmitted(true);
+    console.log(userLogin);
+  };
+
   return (
-    <View>
-      <Text>Login to your profile</Text>
-      <Text>Enter your username</Text>
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={(text) => onChangeText(text)}
-        value={value}
-      />
-      <Text>Enter your password</Text>
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={(text) => onChangeText(text)}
-        value={value}
-      />
-      <TouchableOpacity
-        style={styles.buttonContainer}
-        onPress={() => navigation.navigate("Main")}
-      >
-        <Text style={styles.buttonText}>Log in</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text>Login to your profile</Text>
+        <View>
+          <Text>Enter your email address</Text>
+          <TextInput
+            style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
+        <View>
+          <Text>Enter your password</Text>
+          <TextInput
+            style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+            secureTextEntry={true}
+            keyboardType="default"
+          />
+        </View>
+        <TouchableOpacity style={styles.buttonContainer}>
+          <Button style={styles.buttonText} title="Log in" onPress={submit} />
+          {submitted && navigate("Main")}
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 

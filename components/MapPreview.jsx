929545da4d21112f18d97349fb0ba13434aef588
jakedheath123 <57528439+jakedheath123@users.noreply.github.com>
2020-05-01 17:01:52 +0100

    Map (#5)
    * add map functionality
    
    * add to map functionality
    
    Co-authored-by: sgwicks <60254658+sgwicks@users.noreply.github.com>

---
+++ components/MapPreview.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+import key from "../apiKey/apiKey";
+
+const MapPreview = props => {
+  let imagePreviewUrl;
+
+  if (props.location) {
+    imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.lat},${props.location.lng}&zoom=15&size=200x200&maptype=roadmap&markers=color:red%7Clabel:A%7C${props.location.lat},${props.location.lng}&key=${key.googleApiKey}`;
+  }
+
+  return (
+    <View style={{ ...styles.mapPreview, ...props.style }}>
+      {props.location ? (
+        <Image style={styles.mapImage} source={{ uri: imagePreviewUrl }} />
+      ) : (
+        props.children
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  mapImage: {
+    width: "100%",
+    height: "100%"
+  }
+});
+
+export default MapPreview;
--- screens/MainScreen.jsx
+++ screens/MainScreen.jsx
@@ -1,83 +1,78 @@
-
 import React, { useState, useEffect, Fragment } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { getAllBirdsByArea } from "../apiRequest/apiRequests";
 import Overlay from "react-native-modal-overlay";
 import ImagePicker from "../components/ImagePicker";
+import MapScreen from "./MapScreen";
 
-const MainScreen = (props) => {
-	const [images, updateImages] = useState([]);
-	const [location, updateLocation] = useState("Sleights");
-	const [isLoading, updateIsLoading] = useState(true);
-	const [isVisible, updateIsVisible] = useState(false);
+const MainScreen = props => {
+  const [images, updateImages] = useState([]);
+  const [location, updateLocation] = useState("Sleights");
+  const [isLoading, updateIsLoading] = useState(true);
+  const [isVisible, updateIsVisible] = useState(false);
 
-	const onClose = () => {
-		updateIsVisible(false);
-	};
-    
-    const imageTakenHandler = imagePath => {
-    setImages({ img: imagePath });
+  const onClose = () => {
+    updateIsVisible(false);
   };
-    
-    // useEffect(() => {
-  //   imageTakenHandler(imagePath);
-  // }, [imagePath]);
 
-	const getAreaBirdsUrl = `https://rmx5oedl1b.execute-api.eu-west-2.amazonaws.com/development/birds/${location}`;
+  const getAreaBirdsUrl = `https://rmx5oedl1b.execute-api.eu-west-2.amazonaws.com/development/birds/${location}`;
 
-	useEffect(() => {
-		getAllBirdsByArea(getAreaBirdsUrl)
-			.then((birds) => {
-				updateImages(birds);
-			})
-			.then(() => {
-				updateIsLoading(false);
-			});
-	}, [location]);
+  useEffect(() => {
+    getAllBirdsByArea(getAreaBirdsUrl)
+      .then(birds => {
+        updateImages(birds);
+      })
+      .then(() => {
+        updateIsLoading(false);
+      });
+  }, [location]);
 
-	// useEffect to ask permission for user location (method in react native)
-	// returns a object with longitude and latitude
-	// use longitude/latitude to do stuff using Google Maps API probably
+  // useEffect to ask permission for user location (method in react native)
+  // returns a object with longitude and latitude
+  // use longitude/latitude to do stuff using Google Maps API probably
 
-	if (isLoading)
-		return (
-			<View>
-				<Text>Loading!</Text>
-			</View>
-		);
-	return (
-		<View>
-			<Text>This is the main Screen</Text>
-			{images.map((bird, i) => {
-				return (
-					<Image
-						style={styles.birds}
-						key={i}
-						source={{ uri: bird.img_url }}
-						onPress={() => {
-							updateIsVisible(true);
-						}}
-					/>
-				);
-			})}
-      
-			<Overlay visible={isVisible} onClose={onClose} closeOnTouchOutside>
-        
-				<Text>Lorem ipsum dolor etc.</Text>
-			</Overlay>
-      <ImagePicker onImageTaken={imageTakenHandler} />
-		</View>
-	);
+  if (isLoading)
+    return (
+      <View>
+        <Text>Loading!</Text>
+      </View>
+    );
+  return (
+    <ScrollView>
+      <View>
+        <Text>This is the main Screen</Text>
+
+        {images.map((bird, i) => {
+          console.log(bird);
+          return (
+            <Image
+              style={styles.birds}
+              key={i}
+              source={{ uri: bird.img_url }}
+              onPress={() => {
+                updateIsVisible(true);
+              }}
+            />
+          );
+        })}
+
+        <Overlay visible={isVisible} onClose={onClose} closeOnTouchOutside>
+          <Text>Lorem ipsum dolor etc.</Text>
+        </Overlay>
+        <ImagePicker />
+        <MapScreen />
+      </View>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-	birds: {
-		width: 100,
-		height: 100,
-		// resizeMode: "contain",
-		flex: 1,
-	},
-
+  birds: {
+    width: 100,
+    height: 100,
+    // resizeMode: "contain",
+    flex: 1
+  }
 });
 
 export default MainScreen;

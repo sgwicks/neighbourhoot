ca5226946ad554a635fbb6f9482b9201d4d7d910
sgwicks <samgwicks@gmail.com>
2020-05-01 16:09:24 +0100

    basic BirdOverlay modal functionality

--- components/Overlay.jsx
+++ components/Overlay.jsx
@@ -1,18 +1,20 @@
-import Overlay from "react-native-modal-overlay";
-import React, { Component, Fragment, useState } from "react";
+import Overlay from 'react-native-modal-overlay';
+import React, { Component, Fragment, useState } from 'react';
+import { Text } from 'react-native';
 
-const BirdOverlay = () => {
-	const [isVisible, updateIsVisible] = useState(true);
+const BirdOverlay = ({ navigation }) => {
+  const [isVisible, updateIsVisible] = useState(true);
 
-	const onClose = () => {
-		updateIsVisible(false);
-	};
+  const onClose = () => {
+    updateIsVisible(false);
+    navigation.goBack();
+  };
 
-	return (
-		<Overlay visible={isVisible} onClose={onClose} closeOnTouchOutside>
-			<Text>Lorem ipsum dolor etc.</Text>
-		</Overlay>
-	);
+  return (
+    <Overlay visible={isVisible} onClose={onClose} closeOnTouchOutside>
+      <Text>Lorem ipsum dolor etc.</Text>
+    </Overlay>
+  );
 };
 
 export default BirdOverlay;
